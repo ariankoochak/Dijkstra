6aa029451b6b88c283f282b3ec80c6cd44049806
DULDQ <arian.koochak@gmail.com>
2022-08-13 19:49:23 +0430

    init

--- Dijsktra.cpp
+++ Dijsktra.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
+const int Vertex_num = 6;
+const int Edge_num = 9;
+int Graph[Edge_num][3] = {
+    {0, 1, 7},
+    {0, 2, 9},
+    {0, 5, 14},
+    {1, 2, 10},
+    {1, 3, 15},
+    {2, 3, 11},
+    {2, 5, 2},
+    {3, 4, 6},
+    {4, 5, 9},
+};
 
-int main(){
+int Neighbors[Vertex_num][Vertex_num] = {-1};
+int Dist[Vertex_num] = {10000};
+int Prev[Vertex_num] = {-1};
+int q[Vertex_num];
+int main()
+{
     
 }
