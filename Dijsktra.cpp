98a00565e83531142f1a20c17d24d40863a4b8ab
arian koochak <arian.koochak@gmail.com>
2022-08-15 16:22:03 +0430

    init

--- Dijsktra.cpp
+++ Dijsktra.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
+///////////////////////////////////////Graph Data
 const int Vertex_num = 6;
 const int Edge_num = 9;
 int Graph[Edge_num][3] = {
@@ -15,12 +15,19 @@ int Graph[Edge_num][3] = {
     {3, 4, 6},
     {4, 5, 9},
 };
+/////////////////////////////////////////////////
 
+
+//////////////////////////////////Array & Variable
 int Neighbors[Vertex_num][Vertex_num];
-int Dist[Vertex_num] = {10000};
-int Prev[Vertex_num] = {-1};
+int Dist[Vertex_num];
+int Prev[Vertex_num];
 int q[Vertex_num];
 int origin;
+//////////////////////////////////////////////////
+
+
+/////////////////////////////////////////Functions
 void SetNeighbors(){
     int counter[Vertex_num] = {0};
     for (int i = 0; i < Vertex_num; i++)
@@ -37,17 +44,23 @@ void SetNeighbors(){
         counter[Graph[i][1]]++;
     }
 }
+
+void PrevAndDistSet(){
+    for(int i = 0;i < Vertex_num;i++){
+        Dist[i] = 1000000;
+    }
+    Dist[origin] = 0;
+    Prev[origin] = -1;
+    q[origin] = -1;
+}
+////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////Main
 int main()
 {
+    cin >> origin;
     SetNeighbors();
-    for(int i = 0;i< Vertex_num;i++){
-        for (int j = 0; j < Vertex_num; j++)
-        {
-            cout<<"{"<<Neighbors[i][j]<<" , ";
-        }
-        cout<<"}"<<endl;
-    }
-    cin>>origin;
-    q[origin] = -1;
-    Dist[origin] = 0;
+    PrevAndDistSet();
+
 }
